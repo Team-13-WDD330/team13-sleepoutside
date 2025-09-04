a0d0f360142fcc68e702345d77fc7f87f8c1f8fa
Rafael Pereira de Souza Castro <rcastro2@byupathway.edu>
2025-09-04 19:36:36 -0300

    Add item to cart correction

--- src/js/product.js
+++ src/js/product.js
@@ -4,8 +4,17 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let so_cart = JSON.parse(localStorage.getItem("so-cart"));
+
+  // Se não for um array, inicializa como um array vazio
+  if (!Array.isArray(so_cart)) {
+    so_cart = [];
+  }
+
+  so_cart.push(product);
+  localStorage.setItem("so-cart", JSON.stringify(so_cart));
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
