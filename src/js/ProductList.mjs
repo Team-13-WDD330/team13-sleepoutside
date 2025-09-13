6eaf1007574ee0eaee9b059ec2c4eba3a90a4dff
kpcastillo <krn_iveth09@hotmail.com>
2025-09-13 12:50:17 -0700

    up to date on files

---
+++ src/js/ProductList.mjs
@@ -0,0 +1,32 @@
+import { renderListWithTemplate } from "./utils.mjs";
+
+// template function
+function productCardTemplate(product) {
+  return `
+    <li class="product-card">
+      <a href="../product_pages/index.html?product=${product.Id}">
+        <img src="${product.Image}" alt="${product.Name}" />
+        <h2>${product.Name}</h2>
+        <p>${product.DescriptionHtmlSimple}</p>
+        <p class="price">$${product.FinalPrice}</p>
+      </a>
+    </li>
+  `;
+}
+
+export default class ProductList {
+  constructor(category, dataSource, listElement) {
+    this.category = category;
+    this.dataSource = dataSource;
+    this.listElement = listElement;
+  }
+
+  async init() {
+    const list = await this.dataSource.getData();
+    this.renderList(list);
+  }
+
+  renderList(list) {
+    renderListWithTemplate(productCardTemplate, this.listElement, list, "afterbegin", true);
+  }
+}
