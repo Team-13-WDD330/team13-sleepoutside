b4e4b6b566b7b660f85e08093cf782f5dfb54242
digitalmateus <mnunes@byupathway.edu>
2025-09-13 19:27:42 -0300

    individual w2

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
-import ProductData from "./product_data.js";
-import ProductList from "./ProductList.mjs";
+import ProductData from "product_data.js";
+import ProductList from "ProductList.mjs";
 
 const productData = new ProductData();
 const productList = new ProductList();
