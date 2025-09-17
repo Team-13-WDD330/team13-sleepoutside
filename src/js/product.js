724054cd5f9c093e4bf324c1460185ad6448508e
digitalmateus <mnunes@byupathway.edu>
2025-09-17 14:27:22 -0300

    updated product.js for individual task, show how much is discounted

--- src/js/product.js
+++ src/js/product.js
@@ -1,21 +1,28 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { getParam, getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
+const productID = getParam("product");
 
-function addProductToCart(product) {
+const product = new ProductDetails(productID, dataSource);
+product.init();
+
+  function addProductToCart(productItem) {
   const cartItems = getLocalStorage("so-cart") || [];
-  cartItems.push(product);
+  cartItems.push(productItem);
   setLocalStorage("so-cart", cartItems);
 }
 
 // add to cart button event handler
 async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
+  const productItem = await dataSource.findProductById(e.target.dataset.id);
+  addProductToCart(productItem);
 }
 
+// commented this as it is duplicated in product details page, not sure which to keep
+
 // add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+// document
+//  .getElementById("addToCart")
+//  .addEventListener("click", addToCartHandler);
