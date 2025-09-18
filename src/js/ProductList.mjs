473fec5a2b3cd7b60a0716c568f52828ccb02b80
Rafael Pereira de Souza Castro <rcastro2@byupathway.edu>
2025-09-18 16:19:59 -0300

    W03 Individual Activity: Expand Inventory

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -2,42 +2,37 @@ import { renderListWithTemplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
     return `
-         <li class="product-card">
-            <a href="product_pages/?product=${product.Id}">
-              <img
-                src="${product.Image}" alt="Image of ${product.Name}"
-              />
-              <h3 class="card__brand">${product.Brand.Name}</h3>
-              <h2 class="card__name">${product.Name}</h2>
-             <p class="product-card__price">$${product.FinalPrice}</p>
-            </a>
-        </li>
-        `;
+    <li class="product-card">
+      <a href="/product_pages/?product=${product.Id}">
+        <img src="${product.Images.PrimaryMedium}" alt="${product.Name}">
+        <h3>${product.Brand.Name}</h3>
+        <p>${product.NameWithoutBrand}</p>
+        <p class="product-card__price">$${product.FinalPrice}</p>
+      </a>
+    </li>
+    `;
 }
 
 export default class ProductList {
     constructor(category, dataSource, listElement) {
-        // You passed in this information to make the class as reusable as possible.
-        // Being able to define these things when you use the class will make it very flexible
         this.category = category;
         this.dataSource = dataSource;
         this.listElement = listElement;
     }
 
     async init() {
-
-        const list = await this.dataSource.getData();
+        const list = await this.dataSource.getData(this.category);
         this.renderList(list);
+        document.querySelector(".title").textContent = this.category;
     }
 
-renderList() {
+    renderList(list) {
+        // const htmlStrings = list.map(productCardTemplate);
+        // this.listElement.insertAdjacentHTML("afterbegin", htmlStrings.join(""));
 
-    // const htmlStrings = list.map(productCardTemplate);
-    // this.listElement.insertAdjacentHTML('afterbegin', htmlStrings.join(''));
-    
-    renderListWithTemplate(productCardTemplate, this.listElement, list);
+        // apply use new utility function instead of the commented code above
+        renderListWithTemplate(productCardTemplate, this.listElement, list);
 
     }
 
 }
-
--- src/js/product.js
+++ src/js/product.js
@@ -1,23 +1,11 @@
-import { getLocalStorage, setLocalStorage, getParam } from "./utils.mjs";
+import { getParam, loadHeaderFooter } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
+loadHeaderFooter();
+
 const dataSource = new ProductData("tents");
-const productId = getParam("product");
+const productID = getParam("product");
 
-const product = new ProductDetails(productId, dataSource);
+const product = new ProductDetails(productID, dataSource);
 product.init();
-
-console.log(dataSource.findProductById(productId));
-
-
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
