c38a06d50432c7025b8db6dd3f99c1e65eb00871
Mhiztacsong <joh24038@byui.edu>
2025-09-20 23:43:28 +0100

    Duplicate items in cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -24,12 +24,13 @@ function cartItemTemplate(item) {
     <a href="#">
       <h2 class="card__name">${item.Name}</h2>
     </a>
-    <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-    <p class="cart-card__quantity">qty: 1</p>
+    <p class="cart-card__color">${item.Colors?.[0]?.ColorName || "N/A"}</p>
+    <p class="cart-card__quantity">qty: ${item.quantity || 1}</p>
     <p class="cart-card__price">$${item.FinalPrice}</p>
     <button class="remove-item" data-id="${item.Id}">❌</button>
   </li>`;
 }
+
 
 function addRemoveListeners() {
   const removeButtons = document.querySelectorAll(".remove-item");
