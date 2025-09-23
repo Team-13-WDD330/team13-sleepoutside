997ba0dff09a9b08398f357ab1b292fd538be7c4
digitalmateus <mnunes@byupathway.edu>
2025-09-23 10:59:55 -0300

    individual assigment - sort buttons (name and price)

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,18 +1,16 @@
-import ProductData from './ProductData.js';
-import ProductList from './ProductList.js';
+import ProductData from './ProductData.mjs';
+import ProductList from './ProductList.mjs';
 import { loadHeaderFooter, getParam } from './utils.mjs';
 
 loadHeaderFooter();
 
 const category = getParam('category');
 
-// Get the main element where content will be rendered
 const mainElement = document.querySelector('main');
 
-// Create the product list section with sort controls
 const productListHTML = `
   <section class="products">
-    <h2><span class="title"></span> Products</h2>
+    <h2><span class="title"></span> Products- </h2>
     <div class="sort-controls">
       <button id="sort-by-name">Sort by Name</button>
       <button id="sort-by-price">Sort by Price</button>
@@ -21,14 +19,13 @@ const productListHTML = `
   </section>
 `;
 
-// Insert the product list HTML into the main element
-mainElement.insertAdjacentHTML('beforeend', productListHTML);
 
-// First, create an instance of the ProductData class.
-const dataSource = new ProductData();
-// Then get the element you want the product list to render in
+mainElement.insertAdjacentHTML( 'beforeend', productListHTML);
+
+
 const listElement = document.querySelector('.product-list');
-// Then create an instance of the ProductList class and send it the correct information.
+
+
+const dataSource = new ProductData();
 const myList = new ProductList(category, dataSource, listElement);
-// Finally call the init method to show the products
 myList.init();
